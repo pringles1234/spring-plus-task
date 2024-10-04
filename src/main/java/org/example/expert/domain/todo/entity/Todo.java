dd9118557d809dd75f2261892a7cd27a0c2abca7
Yujin Lee <dbwls9040@gmail.com>
2024-10-05 02:24:39 +0900

    level2-6 : JPA Cascade

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -17,27 +17,28 @@ import java.util.List;
 @Table(name = "todos")
 public class Todo extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String contents;
-    private String weather;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String contents;
+  private String weather;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
-    private List<Comment> comments = new ArrayList<>();
+  @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
+  private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
-    private List<Manager> managers = new ArrayList<>();
+  @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
+  private List<Manager> managers = new ArrayList<>();
 
-    public Todo(String title, String contents, String weather, User user) {
-        this.title = title;
-        this.contents = contents;
-        this.weather = weather;
-        this.user = user;
-        this.managers.add(new Manager(user, this));
-    }
+  public Todo(String title, String contents, String weather, User user) {
+    this.title = title;
+    this.contents = contents;
+    this.weather = weather;
+    this.user = user;
+    this.managers.add(new Manager(user, this));
+  }
 }
